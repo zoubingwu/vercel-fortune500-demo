24454cba37fccb8dc109855d86ae65a3464b00ac
zoubingwu <zoubingwu@gmail.com>
2023-01-17 16:39:33 +0800

    init

---
+++ pages/api/search.js
@@ -0,0 +1,34 @@
+const url =
+  "https://data.tidbcloud.com/api/v1beta/apps/chat2query-nxLGChUR/v1/chat2data";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req, res) {
+  const { searchParams } = new URL(req.url);
+  const query = searchParams.get("q");
+
+  if (req.method === "GET" && query) {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        cluster_id: process.env.CLUSTER_ID,
+        database: process.env.DB,
+        instruction: query,
+      }),
+      headers: {
+        "api-key": process.env.API_KEY,
+      },
+    });
+
+    return new Response(await response.text(), {
+      status: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
+
+  res.status(200).json({ name: "John Doe" });
+}
