46ab3d740ac150b6368fc7788dae8bd67168cddc
张冲 <zhangchong@192.168.1.2>
2023-01-18 18:03:38 +0800

    fix: use env variable

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,24 +1,28 @@
-const url =
-  "https://data.tidbcloud.com/api/v1beta/apps/chat2query-yElaQuwQ/v1/chat2chart";
-
 export const config = {
   runtime: "edge",
 };
+
+const TIDBCLOUD_ENV = {
+  url: process.env.TIDBCLOUD_URL,
+  api_key: process.env.TIDBCLOUD_API_KEY,
+  db: process.env.TIDBCLOUD_DB,
+  cluster_id: process.env.TIDBCLOUD_CLUSTER_ID,
+}
 
 export default async function handler(req, res) {
   const { searchParams } = new URL(req.url);
   const query = searchParams.get("q");
 
   if (req.method === "GET" && query) {
-    const response = await fetch(url, {
+    const response = await fetch(TIDBCLOUD_ENV.url, {
       method: "POST",
       body: JSON.stringify({
-        cluster_id: process.env.CLUSTER_ID,
-        database: process.env.DB,
+        cluster_id: TIDBCLOUD_ENV.cluster_id,
+        database: TIDBCLOUD_ENV.db,
         instruction: query,
       }),
       headers: {
-        "api-key": process.env.API_KEY,
+        "api-key": TIDBCLOUD_ENV.api_key,
       },
     });
 
