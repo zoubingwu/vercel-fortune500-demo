315b5360fd63170135a3f85036947c1603f68d99
张冲 <zhangchong@192.168.1.2>
2023-01-18 12:32:55 +0800

    beauty style

--- components/Charts/TableChart.js
+++ components/Charts/TableChart.js
@@ -1,37 +1,34 @@
 import { Table } from "@mantine/core";
+import style from "./TableChart.module.css";
 
 export const TableChart = ({ chartInfo, data, className, columns, fields }) => {
   const { title } = chartInfo;
   console.log(data);
   console.log("columns", columns);
-  const rows = data.map((element) => (
-    <tr key={element.name}>
-      {Object.values(element).map((v, i) => (
-        <td key={i}>{v}</td>
-      ))}
-    </tr>
-  ));
   const titles = (
     <tr>
       {Object.values(columns).map((value, i) => (
-        <th key={i} style={{ height: "60px", color: "#c4c4c4" }}>
+        <th key={i} className={style.thead_tr_th}>
           {value.col}
         </th>
       ))}
     </tr>
   );
+
+  const rows = data.map((element) => (
+    <tr key={element.name}>
+      {Object.values(element).map((v, i) => (
+        <td key={i} className={style.tbody_tr_td}>
+          {v}
+        </td>
+      ))}
+    </tr>
+  ));
+
   return (
-    <div
-      style={{
-        padding: "0 20px 5px 20px",
-        border: "1px solid #c4c4c4",
-        borderRadius: "3px",
-      }}
-    >
-      <Table style={{ color: "#c4c4c4" }}>
-        <thead style={{ backgroundColor: "black", weight: "700" }}>
-          {titles}
-        </thead>
+    <div className={style.root}>
+      <Table className={style.table}>
+        <thead>{titles}</thead>
         <tbody>{rows}</tbody>
       </Table>
     </div>
